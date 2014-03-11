956ff04847903465ec0e1a1356e70955eae8b859
yorn <git@yorn.priv.no>
2014-03-11 19:12:56 +0100

    Added documentation

--- src/no/ntnu/item/ttm4115/termassignment/clientframe/LogTable.java
+++ src/no/ntnu/item/ttm4115/termassignment/clientframe/LogTable.java
@@ -10,31 +10,51 @@ import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+/**
+ * Three-column table (time in/out message) to show events sent and received.
+ * The table will scroll down if it is aware of its enclosing {@link JScrollPane}.
+ */
 public class LogTable extends JTable {
+	private static final long serialVersionUID = 1L;
 
+	/** Direction of the event (incoming or outgoing) */
 	public enum Direction {IN, OUT}
 
-	private static final long serialVersionUID = 1L;
+	/** Scrollpane that contains this table, used for scrolling down on add */
 	private JScrollPane scrollPane = null;
 
+	/**
+	 * Construct new log table
+	 */
 	public LogTable() {
 		super(new DefaultTableModel(new Object[0][0], new String[]{"Time", "in/out", "Message"}));
 		getColumnModel().getColumn(0).setMaxWidth(SwingUtilities.computeStringWidth(getFontMetrics(getFont()), "MM:MM:MM"));
 		getColumnModel().getColumn(1).setMaxWidth(SwingUtilities.computeStringWidth(getFontMetrics(getFont()), "IN/OUT"));
 	}
 	
+	/** {@inheritDoc}} */
 	public DefaultTableModel getModel() {
 		return (DefaultTableModel) super.getModel();
 	}
+	/** {@inheritDoc}} */
 	public void setModel(TableModel model) throws IllegalArgumentException {
 		if (model instanceof DefaultTableModel) {
 			super.setModel(model);
 		} else {
-			throw new IllegalArgumentException("Requires DefaultTableModel");
+			throw new IllegalArgumentException("LogTable requires DefaultTableModel");
 		}
 	}
 	
+	/**
+	 * Add one event to the log.
+	 * If the scroll pane is known, scroll down.
+	 * 
+	 * @param direction	direction of the event 
+	 * @param message the message describing the event
+	 */
 	public void addLogEntry(Direction direction, String message) {
+		if (direction == null)
+			throw new NullPointerException("Direction");
 		Calendar cal = Calendar.getInstance();
     	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 		getModel().addRow(new Object[]{
@@ -53,6 +73,13 @@ public class LogTable extends JTable {
 		}
 	}
 
+	/**
+	 * Make the table aware of the {@link JScrollPane} that contains it.
+	 * This knowledge is used to scroll the {@link JScrollPane} down when new information is added,
+	 * so that the new information is always visible
+	 * 
+	 * @param scrollPane the {@link JScrollPane} that contains this <code>LogTable</code>
+	 */
 	public void setScrollPane(JScrollPane scrollPane) {
 		if (scrollPane != null) {
 			this.scrollPane = scrollPane;
--- src/no/ntnu/item/ttm4115/termassignment/clientframe/PlaceholderTextField.java
+++ src/no/ntnu/item/ttm4115/termassignment/clientframe/PlaceholderTextField.java
@@ -6,11 +6,21 @@ import java.awt.Graphics2D;
 import javax.swing.FocusManager;
 import javax.swing.JTextField;
 
+/**
+ * A JTextField which displays a gray placeholder text when
+ * not focussed and no text has been entered yet.
+ */
 public class PlaceholderTextField extends JTextField {
 	private static final long serialVersionUID = 1L;
-	
+
+	/** The placeholder text */
 	private String placeholder;
 
+	/**
+	 * Construct a new PlaceholderTextField
+	 * 
+	 * @param placeholder the placeholder text
+	 */
 	public PlaceholderTextField(String placeholder) {
 		this.placeholder = placeholder;
 	}
@@ -24,6 +34,7 @@ public class PlaceholderTextField extends JTextField {
 						.getFocusOwner() == this)) {
 			Graphics2D g2 = (Graphics2D) g.create();
 			g2.setColor(Color.GRAY);
+			// TODO: these values should be calculated from the current theme
 			g2.drawString(placeholder, 5, 15);
 			g2.dispose();
 		}
