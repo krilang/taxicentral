f999913759fd0c1c272e93ca66a7dc4781e96671
Steffen Birkeland <Steffenf.birkeland@gmail.com>
2014-03-25 17:49:44 +0100

    save before, apha test

--- src/no/ntnu/item/ttm4115/termassignment/userclient/UserClient.java
+++ src/no/ntnu/item/ttm4115/termassignment/userclient/UserClient.java
@@ -1,13 +1,16 @@
 package no.ntnu.item.ttm4115.termassignment.userclient;
 
 import no.ntnu.item.arctis.runtime.Block;
+import no.ntnu.item.ttm4115.mqtt.basicmqtt.BasicMQTT.AdvancedConfiguration;
 import no.ntnu.item.ttm4115.termassignment.Status.Status;
 import no.ntnu.item.ttm4115.termassignment.order.Order;
 
 public class UserClient extends Block {
 
-	public no.ntnu.item.ttm4115.termassignment.order.Order current_order;
 	public java.lang.String user_ID;
+	public no.ntnu.item.ttm4115.termassignment.order.Order current_order;
+	
+	
 
 	public static java.lang.String getAlias(String user_ID) {
 		return user_ID;
@@ -24,10 +27,20 @@ public class UserClient extends Block {
 	}
 
 	public void cancel() {
+		if(current_order!=null){
 		current_order.order_status=Status.USER_CANCEL;
+		}else return;
 		
 		
 	}
+
+	public AdvancedConfiguration generateConf() {
+		return new AdvancedConfiguration("test", 99);
+		
+	}
+	
+	
+	
 
 
 
