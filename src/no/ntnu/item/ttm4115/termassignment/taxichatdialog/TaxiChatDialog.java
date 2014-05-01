ee5ae4a157b76a0525fe015da1b0c63ed4301181
krilang <kristian.l.lange@gmail.com>
2014-05-01 17:26:28 +0200

    Revert "Taxichat implemented "
    This reverts commit 8bd76dfb1bc709f4c53d09091f768e537812a660.

--- src/no/ntnu/item/ttm4115/termassignment/taxichatdialog/TaxiChatDialog.java
+++ src/no/ntnu/item/ttm4115/termassignment/taxichatdialog/TaxiChatDialog.java
@@ -8,10 +8,8 @@ import no.ntnu.item.ttm4115.termassignment.order.Order;
 public class TaxiChatDialog extends Block {
 
 	public java.lang.String current_user;
-	public java.lang.String taxi_id;
 
 	public AdvancedConfiguration getAdvConfig(String taxi_id) {
-		this.taxi_id = taxi_id;
 		return new AdvancedConfiguration("t"+taxi_id+"chat", 99);
 	}
 
@@ -34,10 +32,6 @@ public class TaxiChatDialog extends Block {
 
 	public String getTopic() {
 		return "u"+current_user+"chat"; 
-	}
-
-	public String getTaxiID() {
-		return "t"+taxi_id;
 	}
 
 }
